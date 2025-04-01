c23363c34ebcd161338a9302ce6264db4a9aae3e
Peter Seibel <peter@gigamonkeys.com>
2025-04-01 16:19:24 -0700

    Make actual nextGeneration method.

--- test-workshop-assignment.js
+++ test-workshop-assignment.js
@@ -3,9 +3,9 @@
 import { dumpJSON } from './file-util.js';
 import { DB } from 'pugsql';
 import { argv } from 'process';
-import { randomAssignment } from './assignments.js';
 import { mapValues } from './util.js';
 import { WorkshopAssignment } from './workshop-assignment.js';
+import { GA, fittest } from './ga.js';
 
 const { groupBy, entries, fromEntries } = Object;
 
@@ -17,6 +17,9 @@ const except = (property) => {
 };
 
 const db = new DB(argv[2]).addQueries('queries.sql');
+
+const popSize = Number(argv[3]);
+const generations = Number(argv[4]);
 
 // Map of workshop name to limits dicts
 const limits = fromEntries(db.limits().map(({workshop, ...limits}) => [ workshop, limits ]));
@@ -35,14 +38,21 @@ const students = mapValues(studentChoices, (choices, email) => {
   };
 });
 
+const logger = (g, pop) => {
+  console.log(`Generation ${g} - best: ${fittest(pop).fitness}`);
+};
 
 const wa = new WorkshopAssignment(limits, students, 0.01);
 
-const p1 = wa.randomDNA();
-const p2 = wa.randomDNA();
+const ga = new GA(wa, logger);
 
-//wa.foo(p1);
+ga.run(popSize, generations);
 
-console.log(wa.fitness(p1));
-console.log(wa.fitness(p2));
-console.log(wa.fitness(wa.cross(p1, p2)));
+// const p1 = wa.randomDNA();
+// const p2 = wa.randomDNA();
+
+// //wa.foo(p1);
+
+// console.log(wa.fitness(p1));
+// console.log(wa.fitness(p2));
+// console.log(wa.fitness(wa.cross(p1, p2)));
--- workshop-assignment.js
+++ workshop-assignment.js
@@ -1,6 +1,7 @@
-import { sum } from './util.js';
 import { dumpJSON } from './file-util.js';
+import { fitnessWeighted, fittest } from './ga.js';
 import { p, choose, shuffled } from './random.js';
+import { sum } from './util.js';
 
 const { abs, floor, min, max, random } = Math;
 const { entries, keys, values } = Object;
@@ -32,11 +33,11 @@ class WorkshopAssignment {
   }
 
   maybeMutate(gene) {
-    // Mutate by picking a workshop to remove, clearing whatever periods its in
-    // and then randomly refilling those periods the same way we did when we
-    // made the assignment.
-
     if (p(this.mutationRate)) {
+      // Mutate by picking a workshop to remove, clearing whatever periods it's
+      // in, and then randomly refilling those periods the same way we did when
+      // we made the assignment.
+
       const student = this.students[gene.email];
       const mutated = structuredClone(gene);
 
@@ -51,66 +52,17 @@ class WorkshopAssignment {
       // const p = this.students[gene.email].periods.toSorted();
       // console.log(`Mutated\n ${JSON.stringify({p, ...gene}, null, 2)}\nto\n${JSON.stringify({p, ...mutated}, null, 2)}`);
     }
-
     return gene;
   }
 
-
-  foo(dna) {
-    dumpJSON(dna);
-    const assignments = countAssignments(dna, this.limits)
-    dumpJSON(assignments);
-    console.log(constraints(assignments));
-
+  nextGeneration(population) {
+    const parents = fitnessWeighted(population, population.length);
+    const baby = () => this.cross(choose(parents).dna, choose(parents).dna);
+    const next = Array(population.length - 1).fill().map(baby);
+    next.push(fittest(population).dna);
+    return next;
   }
-
 }
-
-/*
- * Make a random assignment for the given student such that every required
- * period is filled exactly once and no workshop is assigned twice. Handles
- * multi-period workshops by assigning them to all periods.
- */
-const randomAssignmentX = (student) => {
-
-  const done = (x) => student.periods.every(p => p in x);
-
-  // Since we eliminate incompatible choices we don't need to check here.
-  const add = (assignment, { period, duration, workshop }) => {
-    const updated = {...assignment};
-    for (let i = 0; i < duration; i++) {
-      updated[period + i] = workshop;
-    };
-    return updated;
-  };
-
-  const overlap = ([a, b], [c, d]) => max(a, c) <= min(b, d);
-
-  const range = (c) => [c.period, c.period + c.duration - 1];
-
-  const incompatible = (c, choice) => {
-    return (
-      choice.workshop === c.workshop || overlap(range(c), range(choice))
-    );
-  }
-
-  const eliminate = (c, choices) => choices.filter(x => !incompatible(c, x));
-
-  const loop = (assignment, left) => {
-    if (done(assignment)) {
-      return assignment;
-    } else if (left.length === 0) {
-      return false;
-    } else {
-      const c = left[0];
-      return (
-        loop(add(assignment, c), eliminate(c, left)) || loop(assignment, left.slice(1))
-      );
-    }
-  };
-
-  return { email: student.email, periods: loop({}, shuffled(student.choices)) };
-};
 
 /*
  * Make a fresh random assignment of choices for a given student.
@@ -122,14 +74,16 @@ const randomAssignment = (student) => {
   };
 };
 
-// The choices must be filtered down to only those choices that can still be
-// assigned, i.e. no choice that require an already assigned period and no
-// choices of workshops that have already been assigned.
 const randomlyFillPeriods = (assigned, { choices, periods }) => {
 
   const workshops = new Set(values(assigned));
 
-  const usable = choices.filter(c => !(c.period in assigned) && !workshops.has(c.workshop))
+  const usable = choices.filter(c => {
+    for (let i = 0; i < c.duration; i++) {
+      if (c.period + i in assigned) return false;
+    }
+    return !workshops.has(c.workshop);
+  });
 
   const done = (x) => periods.every(p => p in x);
 
@@ -169,8 +123,6 @@ const randomlyFillPeriods = (assigned, { choices, periods }) => {
 
   return loop(assigned, shuffled(usable));
 };
-
-
 
 const countAssignments = (dna, limits) => {
   const byWorkshop = {};
